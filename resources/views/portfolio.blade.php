46c4d40a9d870cb647ac45c9b043ac5f08d95038
Shahrizan <shahrizan.learner@gmail.com>
2023-05-02 01:02:10 +0700

    first commit

---
+++ resources/views/portfolio.blade.php
@@ -0,0 +1,99 @@
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Portfolio</title>
+    <!-- Custom Google font-->
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@100;200;300;400;500;600;700;800;900&amp;display=swap" rel="stylesheet" />
+    <!-- Bootstrap icons-->
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css" rel="stylesheet" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+      <link href="style/styles.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="container px-5 my-5">
+      <div class="text-center mb-5">
+        <h1 class="display-5 fw-bolder mb-0"><span class="text-gradient d-inline">Portfolio</span></h1>
+      </div>
+      <div class="row gx-5 justify-content-center">
+        <div class="col-lg-11 col-xl-9 col-xxl-8">
+          <!-- Experience Section-->
+          <section>
+            <div class="d-flex align-items-center justify-content-between mb-4">
+              <h2 class="text-primary fw-bolder mb-0">Experience</h2>
+            </div>
+            <!-- Experience Card 1-->
+            <div class="card shadow border-0 rounded-4 mb-5">
+              <div class="card-body p-5">
+                <div class="row align-items-center gx-5">
+                  <div class="col text-center text-lg-start mb-4 mb-lg-0">
+                    <div class="bg-light p-4 rounded-4">
+                      <div class="text-primary fw-bolder mb-2">2022 - Present</div>
+                      <div class="small fw-bolder">HIMSI UNSRI</div>
+                      <div class="small text-muted">Sekretaris Umum II 2023</div>
+                      <div class="small text-muted">Staff Dinas Kestari 2022</div>
+                    </div>
+                  </div>
+                  <div class="col-lg-8"><div>Mengurus administrasi internal HIMSI, menjadi Sekretaris Pelaksana untuk mendukung terselenggaranya berbagai acara HIMSI, serta menjadi Organizing Committee pada Event Nasional SI FEST 2022.</div></div>
+                </div>
+              </div>
+            </div>
+            <!-- Experience Card 2-->
+            <div class="card shadow border-0 rounded-4 mb-5">
+              <div class="card-body p-5">
+                <div class="row align-items-center gx-5">
+                  <div class="col text-center text-lg-start mb-4 mb-lg-0">
+                    <div class="bg-light p-4 rounded-4">
+                      <div class="text-primary fw-bolder mb-2">2018 - 2020</div>
+                      <div class="small fw-bolder">PASMANTA</div>
+                      <div class="small text-muted">Ketua Sekretaris</div>
+                    </div>
+                  </div>
+                  <div class="col-lg-8"><div>Mengurus administrasi internal PASMANTA, menjadi Sekretaris Pelaksana untuk mendukung terselenggaranya berbagai acara PASMANTA, menjadi Panitia pada Event Provinsi, serta menjuarai beberapa perlombaan.</div></div>
+                </div>
+              </div>
+            </div>
+          </section>
+          <!-- Education Section-->
+          <section>
+            <h2 class="text-secondary fw-bolder mb-4">Education</h2>
+            <!-- Education Card 1-->
+            <div class="card shadow border-0 rounded-4 mb-5">
+              <div class="card-body p-5">
+                <div class="row align-items-center gx-5">
+                  <div class="col text-center text-lg-start mb-4 mb-lg-0">
+                    <div class="bg-light p-4 rounded-4">
+                      <div class="text-secondary fw-bolder mb-2">2021 - Present</div>
+                      <div class="mb-2">
+                      <div class="small fw-bolder">Universitas Sriwijaya</div>
+                      <div class="small text-muted">Sistem Informasi</div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-8"><div>Sistem Informasi menggabungkan beberapa bidang keilmuan, yaitu Ilmu Komputer, Manajemen, dan Bisnis. Jadi, pada jurusan ini saya belajar memanfaatkan ilmu dan teknologi komputer untuk mendukung kebutuhan bisnis.</div></div>
+              </div>
+            </div>
+          </div>
+          <!-- Education Card 2-->
+          <div class="card shadow border-0 rounded-4 mb-5">
+            <div class="card-body p-5">
+              <div class="row align-items-center gx-5">
+                <div class="col text-center text-lg-start mb-4 mb-lg-0">
+                  <div class="bg-light p-4 rounded-4">
+                    <div class="text-secondary fw-bolder mb-2">2018 - 2021</div>
+                    <div class="mb-2">
+                    <div class="small fw-bolder">SMA Negeri 3 Palembang</div>
+                    <div class="small text-muted">Ilmu Pengetahuan Alam</div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-8"><div>Merupakan jurusan yang mempelajari ilmu-ilmu eksakta yang bersifat konkret, nyata, dan dapat dibuktikan dengan berbagai percobaan dan penelitian.</div></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
---
+++ resources/views/profile.blade.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Profile</title>
+    <!-- Custom Google font-->
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@100;200;300;400;500;600;700;800;900&amp;display=swap" rel="stylesheet" />
+    <!-- Bootstrap icons-->
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css" rel="stylesheet" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+      <link href="style/styles.css" rel="stylesheet">
+  </head>
+  <body>
+    <section class="py-5">
+      <div class="container px-5 mb-5">
+        <div class="text-center mb-5">
+          <h1 class="display-5 fw-bolder mb-0"><span class="text-gradient d-inline">Profile</span></h1>
+        </div>
+        <div class="row gx-5 justify-content-center">
+          <div class="col-lg-11 col-xl-9 col-xxl-8">
+            <div class="card overflow-hidden shadow rounded-4 border-0 mb-5">
+              <div class="card-body p-0">
+                <div class="d-flex align-items-center">
+                  <div class="p-5">
+                    <h2 class="fw-bolder">Viola Meiriza</h2>
+                      <table width="430px">
+                        <tr>
+                          <td>Nama Panggilan</td>
+                          <td>:</td>
+                          <td>Viola</td>
+                        </tr>
+                        <tr>
+                          <td>Jenis Kelamin</td>
+                          <td>:</td>
+                          <td>Perempuan</td>
+                        </tr>
+                        <tr>
+                          <td>Tanggal Lahir</td>
+                          <td>:</td>
+                          <td>2 Mei 2003</td>
+                        </tr>
+                        <tr>
+                          <td>Jurusan</td>
+                          <td>:</td>
+                          <td>Sistem Informasi Universitas Sriwijaya</td>
+                        </tr>
+                        <tr>
+                          <td>Alamat</td>
+                          <td>:</td>
+                          <td>Opi Regency 2, Cluster Ruby, Blok B06</td>
+                        </tr>
+                        <tr>
+                          <td>Email</td>
+                          <td>:</td>
+                          <td>meirizaviola@gmail.com</td>
+                        </tr>
+                      </table>
+                  </div>
+                  <img class="img-fluid" src="images/profile.png" width="250" alt="..." />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </body>
+</html>
